3d856ac79289da373a90aa3f1c9fc92d7141422c
Robin Siep <RobinSiep1@gmail.com>
2020-06-15 16:08:12 +0200

    protected route test attempt

--- tests/components/ProtectedRoute.test.jsx
+++ tests/components/ProtectedRoute.test.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import configureStore from 'redux-mock-store';
 import renderer from 'react-test-renderer';
-import Button from '../../src/components/Button';
+import { Provider } from 'react-redux';
+import { MemoryRouter, Switch } from 'react-router-dom';
+import ProtectedRoute from '../../src/components/ProtectedRoute';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Button>Test</Button>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+const mockStore = configureStore([]);
+
+describe('ProtectedRoute', () => {
+  let store;
+  let protectedRoute;
+
+  beforeEach(() => {
+    store = mockStore({
+      auth: {authenticated: false}
+    });
+    protectedRoute = renderer.create(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ProtectedRoute protectedRoute={<p>auth</p>} fallbackComponent={<p>unauth</p>} />
+        </MemoryRouter>
+      </Provider>
+    );
+  });
+
+  it('renders correctly', () => {
+    const tree = protectedRoute.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('correctly handles authenticated', () => {
+    let tree = protectedRoute.toJSON();
+    expect(tree).toMatchSnapshot()
+
+
+    tree = protectedRoute.toJSON();
+    expect(tree).toMatchSnapshot()
+
+
+    tree = protectedRoute.toJSON();
+    expect(tree).toMatchSnapshot()
+  });
+})
