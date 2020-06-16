a51a3246b809f3ebf003a9dac8e48f1480fa124d
Robin Siep <RobinSiep1@gmail.com>
2020-06-16 09:47:59 +0200

    theme reducer tests

---
+++ tests/reducers/theme.test.js
@@ -0,0 +1,21 @@
+import { darkTheme, browserPreferredTheme } from '../../src/theme';
+import themeReducer from '../../src/reducers/theme';
+
+describe("theme reducer", () => {
+  it("should return the initial state", () => {
+    expect(themeReducer(undefined, {})).toEqual({
+      theme: browserPreferredTheme
+    });
+  });
+
+  it("should correctly handle CHANGE_THEME", () => {
+    expect(
+      themeReducer({}, {
+        type: 'CHANGE_THEME',
+        theme: darkTheme
+      })
+    ).toEqual({
+      theme: darkTheme
+    });
+  });
+});
