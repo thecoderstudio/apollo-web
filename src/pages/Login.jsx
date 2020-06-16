30097de44b267782e682d7a18d8b6cbeaef13ba8
Robin Siep <RobinSiep1@gmail.com>
2020-06-16 13:41:55 +0200

    ===

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -111,7 +111,7 @@ class Login extends React.Component {
       { withCredentials: true }
     )
       .then(res => {
-        if (res.status == 200) {
+        if (res.status === 200) {
           this.props.dispatch(loginAction());
         }
       });
