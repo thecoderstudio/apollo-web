112a787e19338a754b2a46632b80fb0592249851
Robin Siep <RobinSiep1@gmail.com>
2020-06-25 15:44:34 +0200

    added editor

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Card from '../components/Card';
+import { Editor, EditorState } from 'draft-js';
 
 const Container = styled(Card)`
   height: 100%;
@@ -8,6 +9,29 @@ const Container = styled(Card)`
   width: 700px;
 `
 
-export default function Terminal(props) {
-  return <Container>Terminal</Container>;
+const StyledEditor = styled(Editor)`
+  height: 100%;
+  width: 100%;
+`;
+
+class Terminal extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onTypeChange = this.onTypeChange.bind(this);
+  }
+
+  onTypeChange(newEditorState) {
+    this.setState({editorState: newEditorState});
+  }
+
+  render() {
+    return (
+      <Container>
+        <StyledEditor editorState={this.state.editorState} onChange={this.onTypeChange}  />
+      </Container>
+    );
+  }
 }
+
+export default Terminal;
