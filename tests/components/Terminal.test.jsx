2aad17c0e81cb9add38af189c1e1e8e4ca04be6b
Robin Siep <RobinSiep1@gmail.com>
2020-07-01 12:53:05 +0200

    test unsuccessful connection writes

--- tests/components/Terminal.test.jsx
+++ tests/components/Terminal.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { darkTheme } from '../../src/theme';
 import { Terminal } from '../../src/components/Terminal';
-import chalk from 'chalk';
+import WS from 'jest-websocket-mock';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -14,29 +14,42 @@ const mockAgent = {
 }
 
 describe('Terminal', () => {
+  const server = new WS(`ws://localhost:1234/agent/a2346886-83ba-442d-9fb7-d024c6274e22/shell`)
+
   beforeEach(() => {
     process.env = {
       APOLLO_WS_URL: 'ws://localhost:1234/'
     };
     Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: jest.fn(), // deprecated
-      removeListener: jest.fn(), // deprecated
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
   });
 
-  it('correctly renders with unsuccessful connection', () => {
+  afterEach(() => {
+    WS.clean();
+  });
+
+  it('correctly handles unsuccessful connection', async () => {
     const spy = jest.spyOn(Terminal.prototype, 'write');
     const container = mount(<Terminal theme={darkTheme} agent={mockAgent} />);
-    const agentName = container.find(Terminal).instance().chalk.hex(darkTheme.primary).bold(mockAgent.name);
+    await server.connected;
+    server.error();
+
+    const chalk = container.find(Terminal).instance().chalk
+    const agentName = chalk.hex(darkTheme.primary).bold(mockAgent.name);
     expect(spy).toHaveBeenCalledWith(`Connecting to agent ${agentName}...\n\r\n`);
+    expect(spy).toHaveBeenCalledWith(chalk.hex(darkTheme.error).bold(
+      "Something went wrong in the connection with the agent."
+    ));
   });
 });
