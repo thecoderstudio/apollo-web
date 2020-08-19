01c14da5ae2d5def6cddcd34ad4f887859c3fed4
Robin Siep <RobinSiep1@gmail.com>
2020-08-19 11:18:19 +0200

    fetch users

--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import UserListItem from './UserListItem';
 
 
 export default class UserList extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.fetchUsers = this.fetchUsers.bind(this);
     this.state = {
       users: []
     }
   }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    axios.get(
+      `${process.env.APOLLO_HTTP_URL}user`,
+      { withCredentials: true }
+    ).then(res => {
+      if (res.status === 200) {
+        this.setState({
+          users: res.data
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         {this.state.users.map(user => (
-          <UserListItem />
+          <UserListItem key={user.id} user={user} />
         ))}
       </div>
     );
