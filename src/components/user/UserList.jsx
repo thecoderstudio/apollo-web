15da0c20145d04389c2525bc03243f83f398f2f6
Robin Siep <RobinSiep1@gmail.com>
2020-08-19 10:59:35 +0200

    initial user list state

--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -3,7 +3,19 @@ import UserListItem from './UserListItem';
 
 
 export default class UserList extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
   render() {
-    return <UserListItem />;
+    return (
+      <div>
+        {this.state.users.map(user => (
+          <UserListItem />
+        ))}
+      </div>
+    );
   }
 }
