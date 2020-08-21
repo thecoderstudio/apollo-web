debc936e5967146a2a835d320131e18d50fbed21
Robin Siep <RobinSiep1@gmail.com>
2020-08-21 13:46:39 +0200

    cache current user on login

--- src/reducers/current-user.js
+++ src/reducers/current-user.js
@@ -3,6 +3,7 @@ import { CACHE_CURRENT_USER, REMOVE_CURRENT_USER } from '../actions/current-user
 export default function currentUserReducer(state={}, action) {
   switch(action.type) {
     case CACHE_CURRENT_USER:
+      console.log(action.user);
       return action.user;
     case REMOVE_CURRENT_USER:
       return {};
