f3c188ea9698110aabfe8f7d37c341190cf4a836
Robin Siep <RobinSiep1@gmail.com>
2020-08-21 15:00:04 +0200

    reducer tests

---
+++ tests/reducers/current-user.test.js
@@ -0,0 +1,22 @@
+import { cacheCurrentUser, removeCurrentUser } from '../../src/actions/current-user';
+import currentUserReducer from '../../src/reducers/current-user';
+
+describe("current user reducer", () => {
+  it("should return empty as initial state", () => {
+    expect(currentUserReducer(undefined, {})).toEqual({})
+  });
+
+  it("should correctly CACHE_CURRENT_USER", () => {
+    const user = {
+      name: 'test'
+    }
+    expect(currentUserReducer({}, cacheCurrentUser(user))).toEqual(user);
+  });
+
+it("should correctly REMOVE_CURRENT_USER", () => {
+    const user = {
+      name: 'test'
+    }
+    expect(currentUserReducer(user, removeCurrentUser())).toEqual({});
+  });
+});
