eeaf2fbb4b8d6861bbd854ef689ad7327dae0754
Robin Siep <RobinSiep1@gmail.com>
2020-08-21 15:25:09 +0200

    user list item test

---
+++ tests/components/UserListItem.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import UserListItem from '../../src/components/user/UserListItem';
+
+function getComponent(user) {
+  return renderer.create(
+    <UserListItem user={user} />
+  );
+}
+
+describe("user list item", () => {
+  it("render correctly without role", () => {
+    const user = {
+      username: 'test',
+      role: null
+    };
+    const tree = getComponent(user).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  it("render correctly with role", () => {
+    const user = {
+      username: 'test',
+      role: {
+        name: 'admin'
+      }
+    };
+    const tree = getComponent(user).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+});
