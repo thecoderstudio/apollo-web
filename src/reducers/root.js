6c896c13f60c44335927f6e3ae21aa43cde9eb65
Robin Siep <RobinSiep1@gmail.com>
2020-08-21 13:34:41 +0200

    current user redux

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
+import currentUserReducer from './current-user';
 import themeReducer from './theme';
 
 export default combineReducers({
   authenticated: authReducer,
+  currentUser: currentUserReducer,
   theme: themeReducer
 });
