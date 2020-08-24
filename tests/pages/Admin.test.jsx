229a87e76addbd50b4e2600361d7db3bf010bb11
Robin Siep <RobinSiep1@gmail.com>
2020-08-24 09:29:56 +0200

    admin render test

---
+++ tests/pages/Admin.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import axios from 'axios';
+import waitForExpect from 'wait-for-expect';
+import Admin from '../../src/pages/Admin';
+
+jest.mock('axios');
+
+function getComponent() {
+  return renderer.create(<Admin />);
+}
+
+describe("user list", () => {
+  beforeEach(() => {
+    process.env = {
+      APOLLO_HTTP_URL: 'http://localhost:1234/'
+    };
+  });
+
+  it("renders correctly", async () => {
+    const users = [
+      {
+        username: 'test',
+        role: null
+      },
+      {
+        username: 'admin',
+        role: {
+          name: 'admin'
+        }
+      }
+    ];
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: users
+    });
+
+    const component = getComponent()
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    await waitForExpect(() => {
+      expect(axios.get).toHaveBeenCalled();
+    });
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
