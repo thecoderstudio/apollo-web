762ec06e9d8aafa49638bf71de47e1ce634bd126
rikvanderwerf <rikvanderwerf@invyte.app>
2020-08-27 15:33:29 +0200

    add overlay

---
+++ src/components/modals/ModalOverlay.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const Overlay = styled.div`
+  display: inline;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${props => props.theme.overlay};
+`;
+
+export default function ModalOverlay(props) {
+  return (
+    <Overlay>
+      {props.children}
+    </Overlay>
+  );
+}
+
+ModalOverlay.propTypes = propTypes;
