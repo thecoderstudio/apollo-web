7ae0106ad7fd0658a6e9b897dc5609cf5dac4a1e
Robin Siep <RobinSiep1@gmail.com>
2020-08-27 16:13:11 +0200

    added error field

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  height: 100%;
+`;
+
 const StyledInput = styled.input`
+  min-height: 50px;
+  width: 100%;
   background: ${props => props.theme.white};
   border: none;
   border-radius: 5px;
@@ -11,8 +17,16 @@ const StyledInput = styled.input`
   opacity: 0.9;
 `;
 
+const Error = styled.p`
+  color: ${props => props.theme.error};
+`;
+
 export default function Input(props) {
+  const { className, ...rest } = props;
   return (
-    <StyledInput {...props} />
+    <Container className={className}>
+      <StyledInput {...rest} />
+      <Error>Test</Error>
+    </Container>
   );
 };
