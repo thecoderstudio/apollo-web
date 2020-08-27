d96cc7989fbb955e2c9b89d5f9ae2f39a0801982
rikvanderwerf <rikvanderwerf@invyte.app>
2020-08-27 11:58:13 +0200

    removed import

--- src/components/modals/ConfirmationModal.jsx
+++ src/components/modals/ConfirmationModal.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Modal from './Modal';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import OutlinedButton from '../buttons/OutlinedButton';
