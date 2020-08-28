a29627b5fe0288424f146c8185d9aa27eea9191e
Robin Siep <RobinSiep1@gmail.com>
2020-08-28 13:34:23 +0200

    validation and error binding in create user

--- src/validation/login.js
+++ src/validation/login.js
@@ -3,8 +3,8 @@ import * as Yup from 'yup';
 const requiredError = "Required";
 
 const loginSchema = Yup.object().shape({
-  username: Yup.string().required(requiredError),
-  password: Yup.string().required(requiredError)
+  username: Yup.string().trim().required(requiredError),
+  password: Yup.string().trim().required(requiredError)
 });
 
 export default loginSchema;
