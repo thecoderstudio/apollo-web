1478aace253331056893e79d61d538dc3436bb5d
rikvanderwerf <rikvanderwerf@invyte.app>
2020-09-15 12:24:38 +0200

    codacy

--- src/actions/change-password.js
+++ src/actions/change-password.js
@@ -1,7 +1,5 @@
 export const PROMPTED_PASSWORD_CHANGE = 'PROMPTED_PASSWORD_CHANGE';
 
-export function promptedPasswordChange() {
-  return {
-    type: PROMPTED_PASSWORD_CHANGE
-  };
-}
+export const promptedPasswordChange = () => ({
+  type: PROMPTED_PASSWORD_CHANGE
+});
