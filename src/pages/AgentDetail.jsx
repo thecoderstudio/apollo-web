8192238c618246971dbdc5443cda410555ac5cff
Robin Siep <RobinSiep1@gmail.com>
2020-09-21 11:57:38 +0200

    get agent by id

--- src/pages/AgentDetail.jsx
+++ src/pages/AgentDetail.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class AgentDetail extends React.PureComponent {
+class AgentDetail extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    const { match: { params } } = this.props;
+    this.state = {
+      agent: this.props.agents.get(params.agentId)
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.agents !== this.props.agents) {
+      const { match: { params } } = this.props;
+      this.setState({
+        agent: this.props.agents.get(params.agentId)
+      });
+    }
+  }
+
   render() {
-    return <div></div>
+    return <div>{this.state.agent.name}</div>
   }
 }
+
+export default connect(
+  state => ({agents: state.agent})
+)(AgentDetail);
