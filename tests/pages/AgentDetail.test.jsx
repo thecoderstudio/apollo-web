df40f35a8abc686ad3834ef674b594c83613f52f
Robin Siep <RobinSiep1@gmail.com>
2020-09-24 16:30:48 +0200

    agent detail sync tests

--- tests/pages/AgentDetail.test.jsx
+++ tests/pages/AgentDetail.test.jsx
@@ -4,19 +4,22 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { ThemeProvider } from 'styled-components';
 import axios from 'axios';
+import waitForExpect from 'wait-for-expect';
+import { StatusCodes } from 'http-status-codes';
 import { darkTheme } from '../../src/theme';
 import AgentDetail from '../../src/pages/AgentDetail';
 
 const mockStore = configureStore([]);
+jest.mock('axios');
 
-function getComponent(agent) {
+function getComponentTags(agent) {
   let agents = new Map();
   agents.set(agent.id, agent);
   let store = mockStore({
     agent: agents
   })
 
-  return renderer.create(
+  return (
     <Provider store={store}>
       <ThemeProvider theme={darkTheme}>
         <AgentDetail 
@@ -31,6 +34,10 @@ function getComponent(agent) {
   );
 }
 
+function getComponent(agent) {
+  return renderer.create(getComponentTags(agent));
+}
+
 
 describe("agent detail", () => {
   const defaultAgent = {
@@ -42,7 +49,65 @@ describe("agent detail", () => {
   }
 
   it("renders correctly", () => {
+    axios.get.mockResolvedValue({
+      status: StatusCodes.OK,
+      data: defaultAgent
+    });
     let tree = getComponent(defaultAgent).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("correctly syncs with remote", async () => {
+    axios.get.mockResolvedValue({
+      status: StatusCodes.OK,
+      data: {
+        'id': 'fakeid',
+        'name': 'remote',
+        'architecture': 'amd64',
+        'connection_state': 'disconnected',
+        'operating_system': 'darwin'
+      }
+    });
+
+    let tree = getComponent(defaultAgent).toJSON();
+
+    await waitForExpect(() => {
+      expect(axios.get).toHaveBeenCalledWith(
+        `${process.env.APOLLO_HTTP_URL}agent/fakeid`,
+        { withCredentials: true }
+      );
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  it("handles sync failure", async () => {
+    axios.get.mockImplementationOnce(() => Promise.reject({
+      response: {
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+        statusText: "Something went wrong",
+        data: {}
+      }
+    }));
+
+    let tree = getComponent(defaultAgent).toJSON();
+
+    await waitForExpect(() => {
+      expect(axios.get).toHaveBeenCalledWith(
+        `${process.env.APOLLO_HTTP_URL}agent/fakeid`,
+        { withCredentials: true }
+      );
+      expect(tree).toMatchSnapshot();
+    });
+  })
+
+  it("deals with property updates correctly", () => {
+    const component = getComponent(defaultAgent);
+    let tree = component.toJSON();
+    const newAgent = Object.assign({}, defaultAgent);
+    newAgent['name'] = 'newname';
+    component.update(getComponentTags(newAgent));
+
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
