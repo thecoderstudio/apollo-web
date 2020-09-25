d1bbeee14d5b37e6ec3cf9337e698be9f0de5be6
Robin Siep <RobinSiep1@gmail.com>
2020-09-25 17:31:26 +0200

    updated reducer tests

--- tests/reducers/agent.test.js
+++ tests/reducers/agent.test.js
@@ -1,6 +1,7 @@
 import { Map as ImmutableMap } from 'immutable';
 import agentReducer from '../../src/reducers/agent';
-import { LIST_AGENTS } from '../../src/actions/agent';
+import { PUT_AGENT, LIST_AGENTS } from '../../src/actions/agent';
+import Agent from '../../src/records/Agent';
 
 describe("agent reducer", () => {
   it("should return empty agent list as initial state", () => {
@@ -8,10 +9,21 @@ describe("agent reducer", () => {
   });
 
   it("should correctly handle agent listing", () => {
-    let agents = [{ id: "id" }];
-    expect(agentReducer({}, {
+    const agents = ImmutableMap({ id: Agent({ id: "id" })});
+    
+    expect(agentReducer(undefined, {
       type: LIST_AGENTS,
       agents
-    })).toEqual(agents);
+    })).toEqual(agents );
+  });
+
+  it("should correctly handle agent PUT", () => {
+    const initialState = ImmutableMap({ "test": { id: "test", name:'old' }}) ;
+    const newAgent = Agent({ id: "test", name:'new' });
+
+    expect(agentReducer(undefined, {
+      type: PUT_AGENT,
+      agent: newAgent
+    })).toEqual(ImmutableMap({ "test": newAgent}));
   });
 });
