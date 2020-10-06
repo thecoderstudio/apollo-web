8116c45eda10001a9d974b6af4f735f5729e6d9f
rikvanderwerf <rikvanderwerf@invyte.app>
2020-10-06 14:53:42 +0200

    removed inverted

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -19,24 +19,24 @@ const Container = styled.div`
 const StyledInput = styled.input`
   min-height: 50px;
   width: 100%;
-  background: ${props => props.inverted ? props.theme.lightBlack : props.theme.white};
-  color: ${props => props.inverted ? props.theme.white : props.theme.lightBlack };
+  background: ${(props) => props.inverted ? props.theme.lightBlack : props.theme.white};
+  color: ${(props) => props.inverted ? props.theme.white : props.theme.lightBlack};
   border: none;
   border-radius: 5px;
   font-family: 'Libre Franklin', sans-serif;
   font-size: 1rem;
   padding-left: 10px;
   opacity: 0.9;
-  border: 1px solid ${props => props.hasError ? props.theme.error : 'transparent'};
+  border: 1px solid ${(props) => props.hasError ? props.theme.error : 'transparent'};
 
   &:focus{
     outline: none;
-    border: 1px solid ${props => props.theme.accent};
+    border: 1px solid ${(props) => props.theme.accent};
   }
 `;
 
 const Error = styled.p`
-  color: ${props => props.theme.error};
+  color: ${(props) => props.theme.error};
   margin-top: 5px;
   font-size: 0.9rem;
 `;
@@ -46,7 +46,7 @@ export default function Input(props) {
   const hasError = error.length > 0;
   return (
     <Container className={className}>
-      <StyledInput inverted hasError={hasError} {...rest} />
+      <StyledInput hasError={hasError} {...rest} />
       { hasError && <Error>{error}</Error>}
     </Container>
   );
