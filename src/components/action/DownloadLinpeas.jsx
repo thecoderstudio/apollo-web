94d5cc7181a60760200167c0befdd32253d475b6
Robin Siep <RobinSiep1@gmail.com>
2020-10-27 09:59:05 +0100

    start of download linpeas

---
+++ src/components/action/DownloadLinpeas.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled(Card)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  min-height: 450px;
+  width: 100%;
+  max-width: 400px;
+  transform: translateY(-50%);
+
+  box-sizing:border-box;
+`;
+
+export default class DownloadLinpeas extends React.PureComponent {
+  render() {
+    return (
+      <Container>
+        test
+      </Container>
+    );
+  }
+}
