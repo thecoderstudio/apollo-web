5ee4af1cd7fc0cb952b62b9280c3df1487b3937e
Robin Siep <RobinSiep1@gmail.com>
2020-10-30 12:55:49 +0100

    download response tests

---
+++ src/util/http.js
@@ -0,0 +1,21 @@
+export function downloadResponse(response, filename=null) {
+  let newFilename = filename;
+  if (!newFilename) {
+    const filenameFromHeader = getFilenameFromHeaders(response.headers);
+    newFilename = filenameFromHeader ? filenameFromHeader : 'response';
+  }
+  const link = document.createElement('a');
+  link.href = window.URL.createObjectURL(new Blob([response.data]));
+  link.download = newFilename;
+  link.click();
+}
+
+function getFilenameFromHeaders(headers) {
+    try {
+      return headers['content-disposition'].split('filename=')[1];
+    } catch (error) {
+      if (!(error instanceof TypeError)) {
+        throw error;
+      }
+    }
+}
---
+++ tests/util/http.test.js
@@ -0,0 +1,51 @@
+import { downloadResponse } from '../../src/util/http';
+
+describe('downloadResponse', () => {
+  it("downloads the response with a name from header", () => {
+    const link = {
+      click: jest.fn()
+    };
+    const response = {
+      headers: {
+        'content-disposition': "attachment; filename=filename.jpg"
+      },
+      data: ''
+    };
+
+    jest.spyOn(document, "createElement").mockImplementation(() => link);
+    global.URL.createObjectURL = jest.fn();
+    downloadResponse(response);
+    expect(link.download).toEqual('filename.jpg');
+    expect(link.click).toHaveBeenCalledTimes(1);
+  });
+
+  it("downloads the response with a default filename", () => {
+    const link = {
+      click: jest.fn()
+    };
+    const response = {
+      data: ''
+    };
+
+    jest.spyOn(document, "createElement").mockImplementation(() => link);
+    global.URL.createObjectURL = jest.fn();
+    downloadResponse(response);
+    expect(link.download).toEqual('response');
+    expect(link.click).toHaveBeenCalledTimes(1);
+  });
+
+  it("downloads the response with a given name", () => {
+    const link = {
+      click: jest.fn()
+    };
+    const response = {
+      data: ''
+    };
+
+    jest.spyOn(document, "createElement").mockImplementation(() => link);
+    global.URL.createObjectURL = jest.fn();
+    downloadResponse(response, 'apollo-agent.bin');
+    expect(link.download).toEqual('apollo-agent.bin');
+    expect(link.click).toHaveBeenCalledTimes(1);
+  });
+});
